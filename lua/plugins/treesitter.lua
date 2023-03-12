555b25e9e5b656d25ec72d97c9420c169c97dae2
Aleksiuu <aleksi.uusitalo@siili.com>
2023-03-12 17:11:58 +0200

    Complete rewrite of nvim config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "go",
+        "tsx",
+        "typescript",
+        "yaml",
+      },
+    },
+  },
+}
